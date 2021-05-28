2e1b8e56e6788a77750ba830dc6c95ea65bbe5ed
Emil Madsen <sovende@gmail.com>
2021-05-28 11:35:59 +0200

    Added version test

--- tests/test_ra_fixture_generator.py
+++ tests/test_ra_fixture_generator.py
@@ -3,8 +3,11 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # --------------------------------------------------------------------------------------
+from importlib.metadata import version  # type: ignore
+
 from ra_fixture_generator import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    pyproject_version = version("ra-fixture-generator")
+    assert __version__ == pyproject_version
